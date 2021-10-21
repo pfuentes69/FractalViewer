f96eb340831f375d1a3b7ec1ff618c5929622b99
Pedro F <34808198+pfuentes69@users.noreply.github.com>
2021-10-21 14:15:27 +0200

    Add files via upload

---
+++ src/FractalViewerMenus.h
@@ -0,0 +1,1 @@
+/***** * objBullMenus.h * *		Public interfaces for objBullMenus.c * *****/#pragma once	// include this header				// only once per source filevoid HandleMenu (long mSelect);void AdjustMenus(void);void SetUpMenus(void);
